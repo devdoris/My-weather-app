335209be59377434622826de0f2e3848272c80f7
devdoris <devdoris1@gmail.com>
2025-04-15 21:04:50 +0100

    Fixed a bug.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ function handleSearchSubmit(event) {
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchInput.value = "";
 }
 
 let searchFormElement = document.querySelector("#search-form");
