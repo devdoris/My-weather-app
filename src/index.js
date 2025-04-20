f572f3b8574e730441674caad473a7460c9cd47d
devdoris <devdoris1@gmail.com>
2025-04-20 15:02:53 +0100

    Added my apiKey.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function searchCity (city) {
+    let apiKey = "5baff0a953ab57014b25c7222et9c7o4";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    console.log(apiUrl);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
     searchInput.value = "";
 }
 
